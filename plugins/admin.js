a4c4f54c46b72642ebe19c03ef1796d912a15f4b
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-05-23 11:08:08 +0530

    Update admin.js

--- plugins/admin.js
+++ plugins/admin.js
@@ -125,11 +125,10 @@ Asena.addCommand({pattern: 'demote ?(.*)', fromMe: true, onlyGroup: true, desc: 
     }
 }));
 
-Asena.addCommand({pattern: 'mute ?(.*)', fromMe: true, onlyGroup: true, desc: Lang.MUTE_DESC, dontAddCommandList: true}, (async (message, match) => {    
+Asena.addCommand({pattern: 'mute ?(.*)', fromMe: true, onlyGroup: true, desc: Lang.MUTE_DESC}, (async (message, match) => {    
     var im = await checkImAdmin(message);
     if (!im) return await message.client.sendMessage(message.jid,Lang.IM_NOT_ADMIN,MessageType.text);
-   
-    
+
     if (Config.MUTEMSG == 'default') {
         await message.client.groupSettingChange(message.jid, GroupSettingChange.messageSend, true);
         await message.client.sendMessage(message.jid,Lang.MUTED,MessageType.text);
